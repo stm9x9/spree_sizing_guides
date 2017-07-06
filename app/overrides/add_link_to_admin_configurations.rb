06c81990af490cc9185776c960d5f6033dadc5c5
stm9x9 <github.20.stm@spamgourmet.com>
2017-07-06 22:30:12 +0200

    Update add_link_to_admin_configurations.rb

--- app/overrides/add_link_to_admin_configurations.rb
+++ app/overrides/add_link_to_admin_configurations.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_product_tabs",
                      :name => "sizing_guides_admin_configurations_menu",
-                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
+                     :insert_bottom => "[data-hook='admin_product_tabs']",
                      :text => "<%= configurations_sidebar_menu_item Spree.t(:sizing_guides), admin_sizing_guides_url %>",
                      :disabled => false)
